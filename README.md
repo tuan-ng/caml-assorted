fa76745b1bed7e4c47e93af5f7a1cb7975fabd79
tuan-ng <iam.tuan.ng@gmail.com>
2019-10-31 23:49:48 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OCaml/Caml Light Programs
---
+++ hamming.ml
@@ -0,0 +1,21 @@
+(* A Hamming number is a number divisible only by 2, 3, and 5 *)
+
+let hamming n = 
+  let rec is_hamming = function 
+    n when n mod 2 = 0 -> is_hamming (n/2)
+  | n when n mod 3 = 0 -> is_hamming (n/3)
+  | n when n mod 5 = 0 -> is_hamming (n/5)
+  | 1 -> true 
+  | _ -> false 
+  in if n = 1 then false else is_hamming n ;;
+
+(* If we treat 1 as a hamming number also then the code simplifies a bit *)
+
+let rec hamming = function 
+    n when n mod 2 = 0 -> hamming (n/2)
+  | n when n mod 3 = 0 -> hamming (n/3)
+  | n when n mod 5 = 0 -> hamming (n/5)
+  | 1 -> true 
+  | _ -> false ;;
+
+
